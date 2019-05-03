254b5dee826bbdea22d114b248181cf7bb5ab96b
Morgan Schweers <mschweers@salesforce.com>
2019-05-03 13:55:14 -0700

    Clean up and make more ruby-ish the attribute information lookup.

--- app/hello_fs.rb
+++ app/hello_fs.rb
@@ -2,6 +2,9 @@ class HelloFs
   HELLO_PATH='/hello.txt'
 
   def initialize
+    @body = "Hello, world!\n".dataUsingEncoding(NSUTF8StringEncoding)
+    icon_file = NSBundle.mainBundle.pathForResource("hellodoc", ofType:"icns")
+    @icon_data = NSData.dataWithContentsOfFile(icon_file)
   end
 
   def contentsOfDirectoryAtPath(path, error: error)
@@ -9,21 +12,18 @@ class HelloFs
   end
 
   def contentsAtPath(path)
-    if path == HELLO_PATH
-      "Hello, world!\n".dataUsingEncoding(NSUTF8StringEncoding)
-    end
+    @body if path == HELLO_PATH
+  end
+
+  def attributesOfItemAtPath(path, userData: userData, error: error)
+    { NSFileSize => @body.length, NSFileType => NSFileTypeRegular  } if path == HELLO_PATH
   end
 
   def finderAttributesAtPath(path, error: error)
-    if path == HELLO_PATH
-      finderFlags = NSNumber.numberWithLong(KHasCustomIcon)
-      {KGMUserFileSystemFinderFlagsKey => finderFlags}
-    end
+    { KGMUserFileSystemFinderFlagsKey => KHasCustomIcon } if path == HELLO_PATH
   end
 
   def resourceAttributesAtPath(path, error: error)
-    if path == HELLO_PATH
-      NSBundle.mainBundle.pathForResource("hellodoc", ofType:"icns")
-    end
+    { KGMUserFileSystemCustomIconDataKey => @icon_data } if path == HELLO_PATH
   end
 end
